62dec553db8271f33c0cbf393fd3e02f367a799f
DESKTOP-J961S7V\Quentin <plionquentin@gmail.com>
2020-11-16 21:14:49 +0100

    Update registration.php password and password_confirmation

--- application/views/authentication/registration.php
+++ application/views/authentication/registration.php
@@ -5,7 +5,7 @@
 		<meta name="robots" content="noindex, nofollow"  />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<?php echo css_url('css/perfect-scrollbar.min.css'); ?>
-		<?php echo css_url('plugins/vuetify/vuetify.css'); ?>
+		<?php echo css_url('plugins/vuetify/vuetify.min.css'); ?>
 		<?php echo css_url('css/style.css'); ?>
 		<link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
 		<?php echo css_url('css/materialdesignicons.min.css'); ?>
@@ -37,8 +37,24 @@
 										<v-form ref="form" class="form-horizontal"  action="<?php echo site_url('registration/create'); ?>" method="post" id="loginform">
 											<v-text-field type="text" id="registerFormInputName" name="name" label="Name"></v-text-field>
 											<v-text-field type="email" id="registerFormInputEmail" name="email" label="Email address"></v-text-field>
-											<v-text-field type="password" id="registerFormInputPassword" name="password" label="Password"></v-text-field>
-											<v-text-field type="password" id="registerFormInputPasswordConfirm" name="password_confirm" label="Password (Confirm)"></v-text-field>
+											<v-text-field
+											:append-icon="show_password ? 'mdi-eye' : 'mdi-eye-off'"
+											:rules="[rules.required, rules.min]"
+											:type="show_password ? 'text' : 'password'"
+											hint="At least 8 characters"
+											@click:append="show_password = !show_password"
+											id="registerFormInputPassword"
+											name="password"
+											label="Password"></v-text-field>
+											<v-text-field
+											:append-icon="show_password_confirmation ? 'mdi-eye' : 'mdi-eye-off'"
+											:rules="[rules.required, rules.min]"
+											:type="show_password_confirmation ? 'text' : 'password'"
+											hint="At least 8 characters"
+											@click:append="show_password_confirmation = !show_password_confirmation"
+											id="registerFormInputPasswordConfirm"
+											name="password_confirm"
+											label="Password (Confirm)"></v-text-field>
 											<v-row>
 												<v-col cols="12" md="9">
 													<v-text-field type="text" name="captcha" label="Code"></v-text-field>
@@ -48,28 +64,37 @@
 												</v-col>
 											</v-row>
 											<div class="terms-conditions row align-items-center justify-content-center pt-4 mb-8">
-												<v-checkbox name="accept_terms"></v-checkbox>
-												<div>
-													<span>I read and accept</span>
-													<a href="<?php echo site_url('terms-and-conditions'); ?>" class="text-secondary mb-1">terms and conditions</a>
-												</div>
+												<v-checkbox name="accept_terms">
+													<template v-slot:label>
+														<div>
+															I read and accept
+																<a
+																target="_blank"
+																href="<?php echo site_url('terms-and-conditions'); ?>"
+																@click.stop
+																>
+																terms and conditions
+																</a>
+														</div>
+													</template>
+												</v-checkbox>
 											</div>
 											<v-row justify="center">
 												<v-btn type="submit" color="primary" x-large>CREATE MY ACCOUNT</v-btn>
 											</v-row>
 											<?php if($this->session->flashdata('success')){ ?>
-											<div class="alert alert-success">
-												<?php echo $this->session->flashdata('success'); ?> <a class="close" data-dismiss="alert">×</a>
-											</div>
+												<v-alert type="success" dismissible>
+													<?php echo $this->session->flashdata('success'); ?> <a class="close" data-dismiss="alert">×</a>
+												</v-alert>
 											<?php } ?>
 											<?php if($this->session->flashdata('danger')){ ?>
-											<div class="alert alert-danger">
-												<?php echo $this->session->flashdata('danger'); ?> <a class="close" data-dismiss="alert">×</a>
-											</div>
+												<v-alert type="error" dismissible>
+													<?php echo $this->session->flashdata('danger'); ?>
+												</v-alert>
 											<?php } ?>
 
 											<?php if(validation_errors()){
-												echo validation_errors('<div class="alert alert-danger">', ' <a class="close" data-dismiss="alert">×</a></div>');
+												echo validation_errors('<v-alert type="error" dismissible>', '</v-alert>');
 											} ?>
 										</v-form>
 										<div class="login d-flex flex-column flex-sm-row align-items-center justify-content-center mt-8 mb-6 mx-auto">
@@ -87,13 +112,22 @@
 				</v-app>
 			</div>
 		</main>
-		<?php echo js_url('plugins/vue.js'); ?>
-		<?php echo js_url('plugins/vuetify/vuetify.js'); ?>
+		<?php echo js_url('plugins/vue.min.js'); ?>
+		<?php echo js_url('plugins/vuetify/vuetify.min.js'); ?>
 		<?php echo js_url('plugins/axios.min.js'); ?>
 		<script type="text/javascript">
 		var v = new Vue({
 			el: '#app',
 			vuetify: new Vuetify(),
+			data : {
+				show_password: false,
+				show_password_confirmation: false,
+				rules: {
+					required: value => !!value || 'Required.',
+					min: v => v.length >= 8 || 'Min 8 characters',
+					emailMatch: () => (`The email and password you entered don't match`),
+				},
+			},
 			created(){
 				this.displayPage();
 			},
